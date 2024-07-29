d309560a3551b81d0ef86e6670115d8a0dbe9dcb
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-07-29 20:49:56 -0300

    10-commit

---
+++ C# - applyingObjectOrientation/Produto.cs
@@ -0,0 +1,35 @@
+﻿
+class Produto
+{
+    private double price;
+    private int stocks;
+    public string Name { get; set; }
+    public string Brand { get; set; }
+    public double Price
+    {
+        get => price;
+        set
+        {
+            if (value > 0)
+                price = value;
+            else
+                price = 10;
+        }
+    }
+
+    public int Stock
+    {
+        get => stocks;
+        set
+        {
+            if (value > 0)
+                stocks = value;
+            else
+                stocks = 0;
+        }
+    }
+
+    public string ProductInfo => $"{this.Name} {this.Brand} - {this.Price}";
+
+}
+
