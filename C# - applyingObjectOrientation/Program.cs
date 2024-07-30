259969d4add8ec7627012c0873d6cab5530dacb3
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-07-30 20:09:06 -0300

    16-commit

--- C# - applyingObjectOrientation/Program.cs
+++ C# - applyingObjectOrientation/Program.cs
@@ -21,6 +21,25 @@
 //List<int> numerosPares = numeros.FindAll(numero => numero % 2 == 0);
 //numerosPares.ForEach(numero => Console.WriteLine(numero));
 
+//Holder t = new();
+//Account c = new();
+
+//t.Name = "teste";
+//t.Cpf = "878909890";
+//t.Adress = "Adress test";
+
+//c.Holder = t;
+//c.Agency = 1;
+//c.AccountNumber = 2234;
+//c.Balance = 10000000.0;
+//c.Limit = 100000.0;
+
+//Console.WriteLine($"Name: {c.Holder.Name}");
+//Console.WriteLine($"CPF: {c.Holder.Cpf}");
+//Console.WriteLine($"Adress: {c.Holder.Adress}");
+
+using C____applyingObjectOrientation;
+
 Album album = new Album();
 album.Name = "The opera";
 
