9a5ce55096b74454af6d7ae1d4d47fac67fc5dfd
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-07-30 20:04:32 -0300

    15-commit

---
+++ C# - applyingObjectOrientation/ContaAndTitular.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C____applyingObjectOrientation
+{
+    class Holder
+    {
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+        public string Adress { get; set; }
+    }
+
+    class Account
+    {
+        public Holder Holder { get; set; }
+        public int Agency { get; set; }
+        public int AccountNumber { get; set; }
+        public double Balance { get; set; }
+        public double Limit { get; set; }
+
+        public string Informacoes => $"Account nº {this.AccountNumber}, Agency {this.Agency}, Holder: {this.Holder.Name} - Balance: {this.Balance}";
+    }
+}
