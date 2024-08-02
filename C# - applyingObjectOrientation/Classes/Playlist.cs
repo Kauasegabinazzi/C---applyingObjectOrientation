f6a00fea1d122bac53de99106aff677cde42563a
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-08-01 23:05:56 -0300

    24-commit

---
+++ C# - applyingObjectOrientation/Classes/Playlist.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace C____applyingObjectOrientation.Classes;
+
+class Playlist
+{
+    public List<Musica> Musicas = new List<Musica>();
+    public int Ordem { get; set; }
+    public string Tema { get; set; }
+
+    public void Add(Musica musica)
+    {
+        Musicas.Add(musica);
+    }
+}
+
---
+++ C# - applyingObjectOrientation/Classes/ReprodutorMusical.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace C____applyingObjectOrientation.Classes;
+
+class ReprodutorMusical
+{
+    public bool Reprodução { get; set; }
+    public bool Pause { get; }
+    public bool Avanco { get; set; }
+    public int VolumeControl{ get; set; }
+
+    public ReprodutorMusical(bool reproducao, bool pause, bool avanco, int volume )
+    {
+        Reprodução = reproducao;
+        Pause = pause;
+        Avanco = avanco;
+    }
+}
